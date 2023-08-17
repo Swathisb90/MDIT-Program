cfd173f3e4aa6ff7decf5262418421b86ec8b2f1
Swathisb90 <83953715+Swathisb90@users.noreply.github.com>
2023-08-17 16:22:05 +0530

    Create Week8Assignment10.java

---
+++ Week 8 /Week8Assignment10.java
@@ -0,0 +1,13 @@
+package appium;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+
+import org.openqa.selenium.ScreenOrientation;
+import org.testng.annotations.Test;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+
+public class Week8Assignment10 {
